98fd95f789de8f5e09f67031bac14a66090f2e79
Divakaran <nivedya.divakaran@fiserv.com>
2024-03-15 23:10:16 +0530

    my first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
