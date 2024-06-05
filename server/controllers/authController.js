8a1528396380087ac4cbb8f6c45451778f2f55ff
Nivedya Divakaran <d.nivedya@gmail.com>
2024-06-05 15:08:28 +0530

    create signin api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,11 @@
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res, next) => {
-  console.log(34567890);
   const { username, email, password } = req.body;
-  console.log(req.body);
+  if (!username || !email || !password)
+    return res.status(400).json({ message: "ALl fields are required !!" });
   const newUser = new User({ username, email, password });
   try {
     await newUser.save();
@@ -14,4 +15,22 @@ const signup = async (req, res, next) => {
   }
 };
 
-export { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email }).lean();
+    if (!validUser) return next(errorHandler(404, error.message));
+    // const validPassword = bcrypt.compareSync(password, validUser.password);
+    // if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup, signin };
