99427c0308362893249bc6c64834d399563cfc4b
Nivedya Divakaran <d.nivedya@gmail.com>
2024-06-05 16:21:26 +0530

    Complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "../config/axios";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await axios.post("auth/signin", formData);
+      console.log(res);
+      if (res.success == false) {
+        setLoading(false);
+        setError(res.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      const { message } = error.response.data;
+      setLoading(false);
+      setError(message);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignIn</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account ?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      <p className="text-red-500">{error}</p>
+    </div>
+  );
 }
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,9 +19,10 @@ const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email }).lean();
-    if (!validUser) return next(errorHandler(404, error.message));
+    if (!validUser) return next(errorHandler(404, "User not found"));
     // const validPassword = bcrypt.compareSync(password, validUser.password);
-    // if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    if (password != validUser.password)
+      return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser;
     res
